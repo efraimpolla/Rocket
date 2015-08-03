8dfb643090bacae6633a84d6af42eec9f63156b4
Efraim Polla <efraimpolla@gmail.com>
2015-08-03 11:57:25 -0300

    Add: Background resources and files

--- Rocket/Classes/AppDelegate.cpp
+++ Rocket/Classes/AppDelegate.cpp
@@ -1,13 +1,13 @@
 #include "AppDelegate.h"
-#include "HelloWorldScene.h"
-
+#include "MainLayer.h"
+#include "GameConfig.h"
 USING_NS_CC;
 
 AppDelegate::AppDelegate() {
 
 }
 
-AppDelegate::~AppDelegate() 
+AppDelegate::~AppDelegate()
 {
 }
 
@@ -15,58 +15,68 @@ AppDelegate::~AppDelegate()
 //it will takes effect on all platforms
 void AppDelegate::initGLContextAttrs()
 {
-    //set OpenGL context attributions,now can only set six attributions:
-    //red,green,blue,alpha,depth,stencil
-    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+	//set OpenGL context attributions,now can only set six attributions:
+	//red,green,blue,alpha,depth,stencil
+	GLContextAttrs glContextAttrs = { 8, 8, 8, 8, 24, 8 };
 
-    GLView::setGLContextAttrs(glContextAttrs);
+	GLView::setGLContextAttrs(glContextAttrs);
 }
-
 // If you want to use packages manager to install more packages, 
 // don't modify or remove this function
 static int register_all_packages()
 {
-    return 0; //flag for packages manager
+	return 0; //flag for packages manager
 }
 
 bool AppDelegate::applicationDidFinishLaunching() {
-    // initialize director
-    auto director = Director::getInstance();
-    auto glview = director->getOpenGLView();
-    if(!glview) {
-        glview = GLViewImpl::create("My Game");
-        director->setOpenGLView(glview);
-    }
+	
+	// initialize director
+	auto director = Director::getInstance();
+	auto glview = director->getOpenGLView();
+	if (!glview) {
+		glview = GLViewImpl::create("My Game");
+		director->setOpenGLView(glview);
+	}
 
-    // turn on display FPS
-    director->setDisplayStats(true);
+	glview->setDesignResolutionSize(320, 480, ResolutionPolicy::EXACT_FIT);
 
-    // set FPS. the default value is 1.0/60 if you don't call this
-    director->setAnimationInterval(1.0 / 60);
+	// add folder search paths to find the game's resources
+	std::vector<std::string> searchPath;
+	searchPath.push_back("Images");
+	searchPath.push_back("Fonts");
 
-    register_all_packages();
+	// set search paths
+	FileUtils::sharedFileUtils()->setSearchPaths(searchPath);
 
-    // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
+	// turn on display FPS
+	director->setDisplayStats(true);
 
-    // run
-    director->runWithScene(scene);
+	// set FPS. the default value is 1.0/60 if you don't call this
+	director->setAnimationInterval(1.0 / 60);
 
-    return true;
+	register_all_packages();
+
+	// create a scene. it's an autorelease object
+	auto scene = MainLayer::scene();
+
+	// run
+	director->runWithScene(scene);
+
+	return true;
 }
 
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
 void AppDelegate::applicationDidEnterBackground() {
-    Director::getInstance()->stopAnimation();
+	Director::getInstance()->stopAnimation();
 
-    // if you use SimpleAudioEngine, it must be pause
-    // SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
+	// if you use SimpleAudioEngine, it must be pause
+	// SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
 }
 
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground() {
-    Director::getInstance()->startAnimation();
+	Director::getInstance()->startAnimation();
 
-    // if you use SimpleAudioEngine, it must resume here
-    // SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+	// if you use SimpleAudioEngine, it must resume here
+	// SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
 }
